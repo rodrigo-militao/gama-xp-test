7c3008b465513cfdb4cf307c0958c7e5afda684c
rodrigo-militao <militaodorigo9@gmail.com>
2021-10-21 16:53:20 -0300

    salvar em lista

--- gama-xp-test/Saque.cs
+++ gama-xp-test/Saque.cs
@@ -22,31 +22,39 @@ namespace gama_xp_test
 
         public void realizarSaque(double _withdrawValue)
         {
-            double withdrawValue = _withdrawValue;
-            BankNote currentNote = BankNote.Cem;
-            int bankNotesQuantity = 0;
-
-            while (true)
+            try
             {
-                if (withdrawValue >= (int)currentNote)
+
+                double withdrawValue = _withdrawValue;
+                BankNote currentNote = BankNote.Cem;
+                int bankNotesQuantity = 0;
+
+                while (true)
                 {
-                    withdrawValue -= (int)currentNote;
-                    bankNotesQuantity += 1;
-                    Console.WriteLine($"Nota de R$ {(int)currentNote} requerida");
-                }
-                else
-                {
-                    bankNotesQuantity = 0;
-                    int notePrevIndex = Array.IndexOf(Enum.GetValues(currentNote.GetType()), currentNote);
-                    notePrevIndex -= 1;
-                    currentNote = (BankNote)(Enum.GetValues(currentNote.GetType())).GetValue(notePrevIndex);
-                }
+                    if (withdrawValue >= (int)currentNote)
+                    {
+                        withdrawValue -= (int)currentNote;
+                        bankNotesQuantity += 1;
+                        Console.WriteLine($"Nota de R$ {(int)currentNote} requerida");
+                    }
+                    else
+                    {
+                        bankNotesQuantity = 0;
+                        int notePrevIndex = Array.IndexOf(Enum.GetValues(currentNote.GetType()), currentNote);
+                        notePrevIndex -= 1;
+                        currentNote = (BankNote)(Enum.GetValues(currentNote.GetType())).GetValue(notePrevIndex);
+                    }
 
 
-                if(withdrawValue == 0 || withdrawValue < 5)
-                {
-                    break;
+                    if(withdrawValue == 0 || withdrawValue < 5)
+                    {
+                        break;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Valor inválido");
             }
         }
 
